e0613e3e39bef87f1a7ca875652ddbe6bff9f421
ZackDinerstein <zackdhome@gmail.com>
2019-05-22 17:37:19 +0300

    Created lightbox functionality

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 // Open a video player when the user clicks the 'Video' button
+const createLightboxFunctionality = () => {
+    let vidBtn = document.getElementById('1ffc-vid--open');
+    let lightbox = document.getElementById('lightbox--1ffc');
+    let video = document.getElementById('1ffc-video');
 
-    const openLightboxWhenVideoBtnClicked = () => {
-        let vidBtn = document.getElementById('1ffc-vid--open');
-        let lightbox = document.getElementById('lightbox--1ffc');
+    // Open lightbox when button's clicked
+    vidBtn.onclick = function () {
+        lightbox.classList.add('flex');
+    }
 
-        vidBtn.onclick = function(){
-            lightbox.classList.add('flex');
+    // Close lightbox when user clicks outside of video
+    lightbox.onclick = function (e) {
+        if (e.target != video){
+            console.log('You clicked outside the video');
+            video.pause();
+            lightbox.classList.remove('flex');
+        } else {
+            console.log('You clicked inside the video');
         }
     }
-    
-openLightboxWhenVideoBtnClicked();
+}
+
+createLightboxFunctionality();
 
 
