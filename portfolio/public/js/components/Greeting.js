068791f7e354e18d33a1d713e6ae7433f85f2fa0
ZackDinerstein <zackdhome@gmail.com>
2019-05-25 21:46:00 +0300

    Added JS code to public folder

---
+++ portfolio/public/js/components/Greeting.js
@@ -0,0 +1,11 @@
+<script type="text/babel">
+    class Greeting extends React.Component {
+        render() {
+            return (<p>Hello world</p>);
+        }
+    }
+    ReactDOM.render(
+        <Greeting />,
+        document.getElementById('root')
+    );
+</script>
---
+++ portfolio/public/js/main.js
@@ -0,0 +1,40 @@
+(function(){
+
+    // Open a video player when the user clicks the 'Video' button
+    const makeLightboxesFunctional = () => {
+        let vidBtns = document.querySelectorAll('.btn--open-vid');
+        
+        // Open lightbox when any video button's clicked
+        vidBtns.forEach(function(vidBtn){
+            vidBtn.onclick = function() {
+                let vidName = this.dataset.videoToOpen;
+                let lightbox = document.getElementById(`lightbox-${vidName}`);
+                let video = document.getElementById(`demo-video-${vidName}`);
+
+                console.log(`${vidName}`);
+                console.log(lightbox);
+                console.log(video);
+
+                lightbox.classList.add('flex');
+                allowLightboxToClose(lightbox, video);
+            }
+        });
+    }
+    
+    const allowLightboxToClose = (lightbox, video) => {
+        // console.log(lightbox);
+        // console.log(video);
+
+        // Close lightbox when user clicks outside of video
+        lightbox.onclick = function(e) {
+            if (e.target != video){
+                console.log('You clicked outside the video');
+                video.pause();
+                lightbox.classList.remove('flex');
+            } else {
+                console.log('You clicked inside the video');
+            }
+        }
+    }
+    makeLightboxesFunctional();
+})();
