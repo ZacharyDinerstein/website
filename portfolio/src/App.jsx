35687e5631ce1d0807c3ad9ca50e9a7bb933cd09
Zachary Dinerstein <zackdinerstein@gmail.com>
2020-12-23 07:57:51 +0200

    Moved Navbar to seperate file

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,33 +1,14 @@
 import React from 'react';
+import Navbar from './Navbar/Navbar';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div>
       <div id="root"></div>
 
       {/* <!-- Navigation --> */}
-      <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-        <div className="container">
-          <a className="navbar-brand js-scroll-trigger" href="#page-top">Zachary Dinerstein</a>
-          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-            data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-            aria-label="Toggle navigation">
-            Menu
-            <i className="fa fa-bars"></i>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="navbar-nav text-uppercase ml-auto">
-              <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar/>
 
       {/* <!-- Header --> */}
       <header className="masthead">
---
+++ portfolio/src/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+const Navbar = () => (
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div className="container">
+            <a className="navbar-brand js-scroll-trigger" href="#page-top">Zachary Dinerstein</a>
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                    data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                Menu
+                <i className="fa fa-bars"></i>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+                <ul className="navbar-nav text-uppercase ml-auto">
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+);
+
+export default Navbar;
